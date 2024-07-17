68c22a5b210c9b8e3ce73c4d7613209783bb73de
idstackdev <id.stackdevelopers@gmail.com>
2024-07-17 20:54:34 +0700

    "Added department routes and controller, updated employee index view"

--- src/app/routes/Routes.js
+++ src/app/routes/Routes.js
@@ -4,6 +4,7 @@ const auth = require("../controllers/auth/authController");
 const dashboardController = require("../controllers/dashboard/dashboardController");
 const authMiddleware = require("../middlewares/authMiddleware");
 const employeeController = require("../controllers/dashboard/employee/employeeController");
+const departmentController = require("../controllers/dashboard/departemen/departmentController");
 
 router.get("/", auth.index);
 router.post("/login", auth.login);
@@ -12,5 +13,17 @@ router.get("/logout", auth.logout);
 router.get("/dashboard", authMiddleware, dashboardController.index);
 // employee
 router.get("/employee", authMiddleware, employeeController.index);
+// departemens
+router.get("/departments", authMiddleware, departmentController.index);
+router.get(
+  "/departments/add",
+  authMiddleware,
+  departmentController.renderAddDepartmentForm
+);
+router.post(
+  "/departments/add",
+  authMiddleware,
+  departmentController.addDepartment
+);
 
 module.exports = router;
