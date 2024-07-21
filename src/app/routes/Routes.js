8d81f53db9332c664db2fc5c16e5b59ffca7126b
idstackdev <id.stackdevelopers@gmail.com>
2024-07-21 17:05:24 +0700

    "Added new controllers and routes for roles and permissions, and refactored existing routes to use route chaining."

--- src/app/routes/Routes.js
+++ src/app/routes/Routes.js
@@ -8,69 +8,185 @@ const departmentController = require("../controllers/dashboard/departemen/depart
 const attendanceController = require("../controllers/dashboard/attendance/attendanceController");
 const payrollController = require("../controllers/dashboard/payroll/payrollController");
 const leaveController = require("../controllers/dashboard/leave/leaveController");
+const roleController = require("../controllers/dashboard/rolesAndPermissions/roleController");
+const permissionController = require("../controllers/dashboard/rolesAndPermissions/permissionController");
+const modelHasRoleController = require("../controllers/dashboard/rolesAndPermissions/modelHasRoleController");
+const modelHasPermissionController = require("../controllers/dashboard/rolesAndPermissions/modelHasPermissionController");
+const roleHasPermissionController = require("../controllers/dashboard/rolesAndPermissions/roleHasPermissionController");
 
 router.get("/", auth.index);
 router.post("/login", auth.login);
 router.get("/logout", auth.logout);
 
 router.get("/dashboard", authMiddleware, dashboardController.index);
-// employee
-router.get("/employee", authMiddleware, employeeController.index);
-router.get("/employee/add", authMiddleware, employeeController.create);
-router.post("/employee/add", authMiddleware, employeeController.store);
-router.get("/employee/show/:id", authMiddleware, employeeController.show);
-router.get("/employee/edit/:id", authMiddleware, employeeController.edit);
-router.post("/employee/edit/:id", authMiddleware, employeeController.update);
-router.get("/employee/delete/:id", authMiddleware, employeeController.delete);
 
-// departemens
-router.get("/departments", authMiddleware, departmentController.index);
-router.get("/departments/add", authMiddleware, departmentController.create);
-router.post("/departments/add", authMiddleware, departmentController.store);
-router.get("/departments/edit/:id", authMiddleware, departmentController.edit);
-router.post(
-  "/departments/edit/:id",
-  authMiddleware,
-  departmentController.update
-);
-router.get(
-  "/departments/delete/:id",
-  authMiddleware,
-  departmentController.delete
-);
+// Employee routes
+router
+  .route("/employee")
+  .get(authMiddleware, employeeController.index)
+  .post(authMiddleware, employeeController.store);
+router
+  .route("/employee/add")
+  .get(authMiddleware, employeeController.create)
+  .post(authMiddleware, employeeController.store);
+router.route("/employee/show/:id").get(authMiddleware, employeeController.show);
+router
+  .route("/employee/edit/:id")
+  .get(authMiddleware, employeeController.edit)
+  .post(authMiddleware, employeeController.update);
+router
+  .route("/employee/delete/:id")
+  .get(authMiddleware, employeeController.delete);
 
-// Attendance
-router.get("/attendance", authMiddleware, attendanceController.index);
-router.get("/attendance/add", authMiddleware, attendanceController.create);
-router.post("/attendance/add", authMiddleware, attendanceController.store);
-router.get("/attendance/edit/:id", authMiddleware, attendanceController.edit);
-router.post(
-  "/attendance/edit/:id",
-  authMiddleware,
-  attendanceController.update
-);
-router.get(
-  "/attendance/delete/:id",
-  authMiddleware,
-  attendanceController.delete
-);
+// Department routes
+router
+  .route("/departments")
+  .get(authMiddleware, departmentController.index)
+  .post(authMiddleware, departmentController.store);
+router
+  .route("/departments/add")
+  .get(authMiddleware, departmentController.create)
+  .post(authMiddleware, departmentController.store);
+router
+  .route("/departments/edit/:id")
+  .get(authMiddleware, departmentController.edit)
+  .post(authMiddleware, departmentController.update);
+router
+  .route("/departments/delete/:id")
+  .get(authMiddleware, departmentController.delete);
 
-// Payroll
-router.get("/payroll", authMiddleware, payrollController.index);
-router.get("/payroll/add", authMiddleware, payrollController.create);
-router.post("/payroll/add", authMiddleware, payrollController.store);
-router.get("/payroll/show/:id", authMiddleware, payrollController.show);
-router.get("/payroll/edit/:id", authMiddleware, payrollController.edit);
-router.post("/payroll/edit/:id", authMiddleware, payrollController.update);
-router.get("/payroll/delete/:id", authMiddleware, payrollController.delete);
+// Attendance routes
+router
+  .route("/attendance")
+  .get(authMiddleware, attendanceController.index)
+  .post(authMiddleware, attendanceController.store);
+router
+  .route("/attendance/add")
+  .get(authMiddleware, attendanceController.create)
+  .post(authMiddleware, attendanceController.store);
+router
+  .route("/attendance/edit/:id")
+  .get(authMiddleware, attendanceController.edit)
+  .post(authMiddleware, attendanceController.update);
+router
+  .route("/attendance/delete/:id")
+  .get(authMiddleware, attendanceController.delete);
 
-// Leave
-router.get("/leave", authMiddleware, leaveController.index);
-router.get("/leave/add", authMiddleware, leaveController.create);
-router.post("/leave/add", authMiddleware, leaveController.store);
-router.get("/leave/show/:id", authMiddleware, leaveController.show);
-router.get("/leave/edit/:id", authMiddleware, leaveController.edit);
-router.post("/leave/edit/:id", authMiddleware, leaveController.update);
-router.get("/leave/delete/:id", authMiddleware, leaveController.delete);
+// Payroll routes
+router
+  .route("/payroll")
+  .get(authMiddleware, payrollController.index)
+  .post(authMiddleware, payrollController.store);
+router
+  .route("/payroll/add")
+  .get(authMiddleware, payrollController.create)
+  .post(authMiddleware, payrollController.store);
+router.route("/payroll/show/:id").get(authMiddleware, payrollController.show);
+router
+  .route("/payroll/edit/:id")
+  .get(authMiddleware, payrollController.edit)
+  .post(authMiddleware, payrollController.update);
+router
+  .route("/payroll/delete/:id")
+  .get(authMiddleware, payrollController.delete);
+
+// Leave routes
+router
+  .route("/leave")
+  .get(authMiddleware, leaveController.index)
+  .post(authMiddleware, leaveController.store);
+router
+  .route("/leave/add")
+  .get(authMiddleware, leaveController.create)
+  .post(authMiddleware, leaveController.store);
+router.route("/leave/show/:id").get(authMiddleware, leaveController.show);
+router
+  .route("/leave/edit/:id")
+  .get(authMiddleware, leaveController.edit)
+  .post(authMiddleware, leaveController.update);
+router.route("/leave/delete/:id").get(authMiddleware, leaveController.delete);
+
+// Rple routes
+router
+  .route("/role")
+  .get(authMiddleware, roleController.index)
+  .post(authMiddleware, roleController.store);
+router
+  .route("/role/add")
+  .get(authMiddleware, roleController.create)
+  .post(authMiddleware, roleController.store);
+router
+  .route("/role/edit/:id")
+  .get(authMiddleware, roleController.edit)
+  .post(authMiddleware, roleController.update);
+router.route("/role/delete/:id").get(authMiddleware, roleController.delete);
+
+// Permission routes
+router
+  .route("/permission")
+  .get(authMiddleware, permissionController.index)
+  .post(authMiddleware, permissionController.store);
+router
+  .route("/permission/add")
+  .get(authMiddleware, permissionController.create)
+  .post(authMiddleware, permissionController.store);
+router
+  .route("/permission/edit/:id")
+  .get(authMiddleware, permissionController.edit)
+  .post(authMiddleware, permissionController.update);
+router
+  .route("/permission/delete/:id")
+  .get(authMiddleware, permissionController.delete);
+
+// model Has Role routes
+router
+  .route("/model-has-role")
+  .get(authMiddleware, modelHasRoleController.index)
+  .post(authMiddleware, modelHasRoleController.store);
+router
+  .route("/model-has-role/add")
+  .get(authMiddleware, modelHasRoleController.create)
+  .post(authMiddleware, modelHasRoleController.store);
+router
+  .route("/model-has-role/edit/:id")
+  .get(authMiddleware, modelHasRoleController.edit)
+  .post(authMiddleware, modelHasRoleController.update);
+router
+  .route("/model-has-role/delete/:id")
+  .get(authMiddleware, modelHasRoleController.delete);
+
+// model Has Permission routes
+router
+  .route("/model-has-permission")
+  .get(authMiddleware, modelHasPermissionController.index)
+  .post(authMiddleware, modelHasPermissionController.store);
+router
+  .route("/model-has-permission/add")
+  .get(authMiddleware, modelHasPermissionController.create)
+  .post(authMiddleware, modelHasPermissionController.store);
+router
+  .route("/model-has-permission/edit/:id")
+  .get(authMiddleware, modelHasPermissionController.edit)
+  .post(authMiddleware, modelHasPermissionController.update);
+router
+  .route("/model-has-permission/delete/:id")
+  .get(authMiddleware, modelHasPermissionController.delete);
+
+// role Has Permission routes
+router
+  .route("/role-has-permission")
+  .get(authMiddleware, roleHasPermissionController.index)
+  .post(authMiddleware, roleHasPermissionController.store);
+router
+  .route("/role-has-permission/add")
+  .get(authMiddleware, roleHasPermissionController.create)
+  .post(authMiddleware, roleHasPermissionController.store);
+router
+  .route("/role-has-permission/edit/:id")
+  .get(authMiddleware, roleHasPermissionController.edit)
+  .post(authMiddleware, roleHasPermissionController.update);
+router
+  .route("/role-has-permission/delete/:id")
+  .get(authMiddleware, roleHasPermissionController.delete);
 
 module.exports = router;
